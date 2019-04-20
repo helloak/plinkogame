f23bf4d78c4064acc064ee5f48ec59ed789a1e31
Akarsh Valsan <akarshvalsan@Akarshs-MBP.home>
2019-04-20 05:13:40 +0200

    new push

---
+++ score.js
@@ -0,0 +1,83 @@
+//const predictionPoint = 300;
+//const bucket = console.log('Your point wil be',bucket);
+
+
+
+const outputs = [];
+
+const k=3;
+
+function distance(pointA, pointB){
+  return Math.abs(pointA - pointB);
+}
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+
+  console.log(outputs);
+
+}
+
+function runAnalysis() {
+
+  const [testSet, trainingSet] = splitDataset(outputs,10);
+
+  for (let i=0; i<testSet.length; i++) {
+    const bucket = knn(trainingSet, testSet[i][0]);
+    console.log(bucket, testSet[i][3]);
+
+
+  }
+
+
+
+
+}
+
+function knn(data, point) {
+
+  return _.chain(data)
+  .map(row => [distance(row[0],point),row[3]])
+  .sortBy(row=>row[0])
+  .slice(0,k)
+  .countBy(row=>row[1])
+  .toPairs()
+  .sortBy(row=>row[1])
+  .last()
+  .first()
+  .parseInt()
+  .value();
+
+}
+
+
+
+function splitDataset(data,testCount) {
+
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+  //testCount to end for trainingset
+
+
+  return [testSet, trainingSet];
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
