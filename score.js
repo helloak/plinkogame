f3263ed73823464353498d47ba2d2a7e44e2e4ff
Akarsh Valsan <akarshvalsan@Akarshs-MBP.home>
2019-04-21 03:38:54 +0200

    new edits

--- score.js
+++ score.js
@@ -1,44 +1,58 @@
 //const predictionPoint = 300;
 //const bucket = console.log('Your point wil be',bucket);
-
+//const k=3;
 
 
 const outputs = [];
 
-const k=3;
+
 
 function distance(pointA, pointB){
-  return Math.abs(pointA - pointB);
+
+  return _.chain(pointA)
+    .zip(pointB)
+    .map(([a,b]) => (a-b) ** 2)
+    .sum()
+    .value() ** 0.5;
 }
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 
-  console.log(outputs);
+  //console.log(outputs);
 
 }
 
 function runAnalysis() {
 
-  const [testSet, trainingSet] = splitDataset(outputs,10);
+  const testSetSize=50;
+  const [testSet, trainingSet] = splitDataset(minMax(outputs,3),testSetSize);
 
-  for (let i=0; i<testSet.length; i++) {
-    const bucket = knn(trainingSet, testSet[i][0]);
-    console.log(bucket, testSet[i][3]);
+  _.range(1,20).forEach(k => {
+    const accuracy = _.chain(testSet)
+    .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3])
+    .size()
+    .divide(testSetSize)
+    .value();
 
-
-  }
-
+console.log('For k of ', k, ' Accuracy is', accuracy);
+  });
 
 
 
 }
 
-function knn(data, point) {
+
+
+
+function knn(data, point, k) {
 
   return _.chain(data)
-  .map(row => [distance(row[0],point),row[3]])
+  .map(row => {
+    return [
+    distance(_.initial(row),point), _.last(row)];
+  })
   .sortBy(row=>row[0])
   .slice(0,k)
   .countBy(row=>row[1])
@@ -68,12 +82,27 @@ function splitDataset(data,testCount) {
 }
 
 
+function minMax(data, featureCount) {
+
+ const clonedData = _.cloneDeep(data);
+
+
+  for(let i=0; i<featureCount; i++) {
+    const column = clonedData.map(row=>row[i]);
+
+    const min=_.min(column);
+    const max=_.max(column);
+
+    for(let j=0; j<clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+  }
 
 
 
+return clonedData;
 
-
-
+}
+}
 
 
 
